857a19243bf98f4362fb43520bec98a34114ad32
cjhhgs <462072107@qq.com>
2021-11-02 16:33:40 +0800

    ..

--- lab3/3_2.c
+++ lab3/3_2.c
@@ -6,8 +6,20 @@
 #include <semaphore.h>
 
 sem_t mutex1;
+sem_t mutex_user[3];
 
 time_t end_time;
+int user_num[3]={0};
+
+int if_have_user(){
+    int res = 1;
+    int i;
+    for(i=0;i<3;i++)
+        sem_wait(mutex_user[i]);
+    if(user_num[0]==0&&user_num[1]==0&&user_num[2]==0)
+        res = 0;
+    return res;
+}
 
 void broadcast(){
     while(time(NULL)<end_time){ //达到设定最大时间结束放映
@@ -16,7 +28,17 @@ void broadcast(){
 
         int i = 0;
         for(i=0;1;i = (i+1)%3){
+            //检测影片 i 是否有人要看
+            sem_wait(&mutex_user[i]);
+            if(user_num[i]!=0){//有人要看这部影片，则放映
+
+            }
             
+            else{       //没人排这队，则不放映
+
+            }
+
+
         }
 
     }
